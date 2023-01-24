b539888d48025711f54ab5d0d5b40850c9bcecd4
Jordan Ford <jsford94@gmail.com>
2023-01-24 11:18:17 -0500

    Use smaller mesh for experiments.

--- experiments.sh
+++ experiments.sh
@@ -4,13 +4,13 @@ rm -rf results/
 
 # Slope Experiment
 mkdir -p results/slope05
-./build/planranger -p=0.25 -va=55 -nv=15 -rs=5 meshes/lmp.ply results/slope05/
+./build/planranger -p=0.25 -va=55 -nv=24 -rs=5 meshes/lmp_crop.ply results/slope05/
 
 mkdir -p results/slope10
-./build/planranger -p=0.25 -va=55 -nv=15 -rs=10 meshes/lmp.ply results/slope10/
+./build/planranger -p=0.25 -va=55 -nv=24 -rs=10 meshes/lmp_crop.ply results/slope10/
 
 mkdir -p results/slope15
-./build/planranger -p=0.25 -va=55 -nv=15 -rs=15 meshes/lmp.ply results/slope15/
+./build/planranger -p=0.25 -va=55 -nv=24 -rs=15 meshes/lmp_crop.ply results/slope15/
 
 mkdir -p results/slope20
-./build/planranger -p=0.25 -va=55 -nv=15 -rs=20 meshes/lmp.ply results/slope20/
+./build/planranger -p=0.25 -va=55 -nv=24 -rs=20 meshes/lmp_crop.ply results/slope20/
