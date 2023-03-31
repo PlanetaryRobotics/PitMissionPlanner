32fde8a0b3675123a0c10908bd612e62989ece2a
dgnadkar <dgnadkar@gmail.com>
2023-03-31 17:54:51 -0400

    done with reorganization for now

---
+++ src/plan.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include "terrainmesh.h"
+#include "terrainmap.h"
+#include "priority_queue.h"
+
+#include "path.h"
+#include <Eigen/Dense>
+
+std::pair<std::vector<Path>, int> multipathplan(const SlopeAtlas& slopeAtlas,
+              const TerrainMapFloat& commsMap,
+              const Path::State& start,
+              const std::vector<Path::State>& goals,
+              const int threadID);
+
+std::pair<std::vector<int>,int64_t> solveTSP(const Eigen::MatrixXd& costs, int depot);
+std::vector<int> routeplan(const Eigen::MatrixXd& costs);
+std::vector<std::vector<Path>> planAllPairsSLOW(const std::vector<Path::State>& sites,
+                                                const SlopeAtlas& slopeAtlas,
+                                                const TerrainMapFloat& commsMap);
+std::vector<std::vector<Path>> planAllPairs(const std::vector<Path::State>& sites,
+                                            const SlopeAtlas& slopeAtlas,
+                                            const TerrainMapFloat& commsMap);
+
